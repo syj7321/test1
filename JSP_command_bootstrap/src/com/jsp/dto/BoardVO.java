359d5b6570e2ced05462001f46235f744e1aa25e
syj7321 <thd7321@naver.com>
2023-01-29 17:11:24 +0900

    JSP_command_bootstrap : add BoardVO.java

---
+++ JSP_command_bootstrap/src/com/jsp/dto/BoardVO.java
@@ -0,0 +1,81 @@
+package com.jsp.dto;
+
+import java.util.Date;
+
+public class BoardVO {
+
+	private int bno;
+	private String title;
+	private String writer;
+	private String content;
+	private int viewcnt;
+	private Date regDate;
+	private Date updatedate;
+
+	private int replycnt;
+
+	public int getBno() {
+		return bno;
+	}
+
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getViewcnt() {
+		return viewcnt;
+	}
+
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public Date getUpdatedate() {
+		return updatedate;
+	}
+
+	public void setUpdatedate(Date updatedate) {
+		this.updatedate = updatedate;
+	}
+
+	public int getReplycnt() {
+		return replycnt;
+	}
+
+	public void setReplycnt(int replycnt) {
+		this.replycnt = replycnt;
+	}
+
+}
